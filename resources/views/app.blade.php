826de5de0182a9a0013e198b2362a4486887921b
stepanozhigov <stepan.ozhigov@gmail.com>
2020-12-10 23:03:59 +0200

    prod

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,7 +25,7 @@ Uppercase
 
 @section('gtag.js')
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162968640-6"></script>;
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162968640-6"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
